dfc2f9480c57d9c354f195275dc5082a8fa7e51c
INGCapaDev <ingcapadev@gmail.com>
2024-10-15 07:28:08 -0700

    :bug: [FIX]: Fix Hero section paddings

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -16,7 +16,11 @@ const HeroSection = () => {
           poster={hero}
         ></video>
       </div>
-      <img src={logo} alt="lgo img" />
+      <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black"></div>
+      <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
+        <img src={logo} alt="Logo img" className="w-full p-4" />
+        <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
+      </div>
     </section>
   );
 };
