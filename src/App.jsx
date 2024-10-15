80868723e058eb2e273bc3d3dca01034e435b14f
INGCapaDev <ingcapadev@gmail.com>
2024-10-15 06:58:35 -0700

    :tada: [CHORE](config): Setup Tailwind and Prettier

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="container p-5">
+      <h1 className="text-3xl font-bold text-red-500 underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
